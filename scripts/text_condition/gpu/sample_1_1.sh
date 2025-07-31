fb5b4ceb0405df78f9ba71aa393f7e40d91f0980
tfl <tflchina@gmail.com>
2025-07-31 18:26:37 +0000

    add b1s bs5 run with 1 image 1 step

---
+++ scripts/text_condition/gpu/sample_1_1.sh
@@ -0,0 +1,28 @@
+
+CUDA_VISIBLE_DEVICES=0 torchrun --nnodes=1 --nproc_per_node 1 --master_port 29513 \
+    -m opensora.sample.sample \
+    --model_type "inpaint" \
+    --model_path "./Open-Sora-Plan-v1.3.0/any93x640x640_i2v" \
+    --version v1_3 \
+    --num_frames 1 \
+    --height 352 \
+    --width 640 \
+    --max_hxw 236544 \
+    --crop_for_hw \
+    --cache_dir "../cache_dir" \
+    --text_encoder_name_1 "./mt5-xxl" \
+    --text_prompt examples/cond_prompt.txt \
+    --conditional_pixel_values_path examples/cond_pix_path.txt \
+    --ae WFVAEModel_D8_4x8x8 \
+    --ae_path "./Open-Sora-Plan-v1.3.0/vae" \
+    --save_img_path "./save_path" \
+    --fps 18 \
+    --guidance_scale 7.5 \
+    --num_sampling_steps 1 \
+    --max_sequence_length 512 \
+    --sample_method EulerAncestralDiscrete \
+    --seed 1234 \
+    --num_samples_per_prompt 1 \
+    --rescale_betas_zero_snr \
+    --prediction_type "v_prediction" \
+    --noise_strength 0.0 \
